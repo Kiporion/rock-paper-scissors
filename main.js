51120b45b51bbaba6c72e9ebbb3196c0d568efdd
Kiporion <meho.mustafic@outlook.com>
2023-01-12 14:16:54 +0100

    Finished playRound function

--- main.js
+++ main.js
@@ -20,16 +20,33 @@ const getComputerChoice =()=>
         default:
             computerSelection.log("Error Ocurred");
     }
-    console.log("Computer chose:", computerSelection);
     return computerSelection;
 }
 
 const playRound=(playerSelection, computerSelection)=>{
     let win =`You Win! ${playerSelection} beats ${computerSelection}`;
     let lose =`You Lose! ${computerSelection} beats ${playerSelection}`;
+    let playerUP = playerSelection.toUpperCase();
+    let computerUP = computerSelection.toUpperCase();
+
+    if(playerUP=="ROCK" && computerUP== "SCISSORS"){
+        return win;
+    }else if(playerUP=="ROCK" && computerUP== "PAPER"){
+        return lose;
+    }else if(playerUP=="PAPER" && computerUP== "ROCK"){
+        return win;
+    }else if(playerUP=="PAPER" && computerUP== "SCISSORS"){
+        return lose;
+    }else if(playerUP=="SCISSORS" && computerUP== "PAPER"){
+
+        return win;
+    }else if(playerUP=="SCISSORS" && computerUP== "ROCK"){
+        return lose;
+    }else if(playerUP===computerUP){
+        return `It's a Tie! You both chose ${playerUP}`
+    }
 }
 
 
 getComputerChoice();
-console.log(computerSelection)
-console.log(playerSelection);
+console.log(playRound(playerSelection,computerSelection));
